0d983f0dd3ca0d350e6403bca44d01ad654d0ce4
Malcolm Mason <79657911+malmason@users.noreply.github.com>
2021-08-11 14:19:02 -0400

    Updated footer

--- src/components/FooterSection.js
+++ src/components/FooterSection.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import '../App.css';
+import logo from './images/logo.png'
+
 
 function FooterSection(){
   return (
-    <footer id="main-footer">Copyright &copy 2021</footer>
+    <footer id="main-footer">  
+      <img src={logo} alt="logo" class="footer-logo"/>
+      <p>Copyright &copy; 2021</p>
+    </footer>
   )
 }
 
